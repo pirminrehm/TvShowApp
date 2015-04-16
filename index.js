047c611aea971f27efd54c98c41400beb03aebdc
pirminrehm <pirmin.rehm@7kw.de>
2015-04-16 14:07:51 +0200

    test3

---
+++ index.js
@@ -0,0 +1,12 @@
+var http = require('http');
+
+
+var server = http.createServer(function (request, response) {
+  response.writeHead(200, {"Content-Type": "text/plain"});
+  response.end("Hello Codebitches\n");
+});
+
+server.listen(8080);
+
+
+console.log("Server running at http://127.0.0.1:8080/");
