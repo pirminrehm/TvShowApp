28c33127d564478f0da4557fe1b1e2b91272f247
pirminrehm <pirmin.rehm@7kw.de>
2015-04-22 21:42:26 +0200

    rest

--- server/models/serie.js
+++ server/models/serie.js
@@ -1,61 +1,33 @@
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
-var SerieSchema = new Schema({
+
+
+var EpisodeSchema = new Schema({
+	number: int,
+	watched: Boolean
+});
+
+var SeasonSchema = new Schema({
+	number: String,
+	episodes: [EpisodeSchema]
+});
+
+var SeriesSchema = new Schema({
 	name: String,
 	tvdatabaseID: String,
-	season:String
+	seasons: [SeasonSchema]
 });
 
 
-
-
-
-
-
-
-
-
-
-
-
-var SerieSchema = new Schema({
+var UserSchema = Schema({
+	userID: int,
+	userLoginToken: String,
 	name: String,
-	tvdatabaseID: String,
-	season: String
-});
+	series: [SeriesSchema]
 
 
-
-
-{
-	"name" : "How I Met Your Mother",
-	"tvdatabaseID" : "tdvid346243234234",
-	"season" : {
-				"s1":
-					{
-						"e1":true,
-						"e2":true,
-						"e3":true,
-						"e4":true,
-						"e5":false,
-						"e6":true,
-						"e7":true,
-						"e8":false
-					}
-
-				"s1":
-					{
-						"e1":true,
-						"e2":true,
-						"e3":true,
-						"e4":true,
-						"e5":false,
-					}
-				}
-}
-
-
+})
 
 
 
