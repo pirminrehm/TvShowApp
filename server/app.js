4f294009d8be396d91278df514e54049c5959405
alexanderWallrabenstein <alexander.wallrabenstein@gmail.com>
2015-04-22 20:24:45 +0200

    small changes in app.js

--- server/app.js
+++ server/app.js
@@ -32,9 +32,7 @@ db.once('open', function callback(){
 
 
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(__dirname + '/public/favicon.ico'));
