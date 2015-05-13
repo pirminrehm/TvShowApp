1a69e86ebdc23d4970e303a5b3587c7177ef433d
alexanderWallrabenstein <alexander.wallrabenstein@gmail.com>
2015-05-13 19:15:50 +0200

    merge in app.js

--- server/app.js
+++ server/app.js
@@ -8,15 +8,13 @@ var mongoose = require('mongoose');
 var cors = require('cors');
 
 //require local packaes
-<<<<<<< HEAD
-// var users = require('./routes/usersRoutes');
+
+
 var series = require('./routes/seriesRoutes');
 var config = require('./config.json');
-=======
-var routes = require('./routes/index');
-var users = require('./routes/users');
-//var series = require('./routes/series');
->>>>>>> 980a4020f10c97f21a5fbb44a141ba396388cb57
+
+
+
 
 
 var app = express();
@@ -30,21 +28,11 @@ db.on('error', function callback() {
   console.log('Verbindung zu MongoDB fehlgeschlagen');
 });
 
-<<<<<<< HEAD
+
 db.on('open', function callback() {
   console.log('Verbindung zu MongoDB erfolgreich (Database: ' + config.dbUrl + ")");
 });
 
-
-=======
-db.once('open', function callback(){
-  console.log("Success: MongoDB Verbindung");
-});
-
-
-
-
->>>>>>> 980a4020f10c97f21a5fbb44a141ba396388cb57
 app.use(cors());
 app.use(logger('dev'));
 app.use(bodyParser.json());
@@ -53,15 +41,9 @@ app.use(cookieParser());
 
 
 //use local packages
-<<<<<<< HEAD
 
-// app.use('/user', users);
 app.use('/series', series);
-=======
-app.use('/', routes);
-app.use('/user', users);
-//app.use('/series', series);
->>>>>>> 980a4020f10c97f21a5fbb44a141ba396388cb57
+
 
 
 // catch 404 and forward to error handler
@@ -102,4 +84,4 @@ app.use(function(err, req, res, next) {
 
 module.exports = app;
 
-console.log("Success: End of Neddit");
+console.log("Success: End of TvShowApp");
--- server/controllers/seriesController.js
+++ server/controllers/seriesController.js
@@ -1,10 +1,8 @@
 require('../models/seriesModel');
 var mongoose = require('mongoose');
-<<<<<<< HEAD
+
 // var Series = mongoose.model("Serie");	/*To Do!!*/
-=======
-//var Series = mongoose.model("Serie");	/*To Do!!*/
->>>>>>> 980a4020f10c97f21a5fbb44a141ba396388cb57
+
 var _ = require('underscore');
 var request = require('request');
 
