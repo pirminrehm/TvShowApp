b289a26c9a79e833b819427bcc329f86cdf9cb4a
alexanderWallrabenstein <alexander.wallrabenstein@gmail.com>
2015-05-26 23:40:52 +0200

    implemented: seriesDetails + episodeDetails

--- server/controllers/seriesController.js
+++ server/controllers/seriesController.js
@@ -1,13 +1,16 @@
 require('../models/seriesModel');
 var mongoose = require('mongoose');
 var parseString = require('xml2js').parseString;
-
-// var Series = mongoose.model("Serie");	/*To Do!!*/
-
 var _ = require('underscore');
 var request = require('request');
 
-var config = require('../config.json');
+var tokenController = require('./tokenController');
+
+var Series = mongoose.model('Series');
+
+
+//logging in der console???
+var clog = false;
 
 
 exports.search = function(req, res){
@@ -28,10 +31,69 @@ exports.search = function(req, res){
 
 
 exports.getSeriesDetails = function(req, res){
-	console.log('in getSeriesDetails');
+
+	var seriesId = req.params.seriesId;
+
+	tokenController.verify(req.params.token, function (verified, user) {
+		if (verified) {
+			if(clog) console.log("Access to restricted area granted");
+			
+			Series.findOne({'Series.id':seriesId}, function (errLoadSeries, resultSeries){
+				if(resultSeries && !errLoadSeries){
+					res.send(resultSeries.Series);
+				}
+				else if(resultSeries === null){
+					res.status(500).jsonp({"error":"Error: seriesId not found"});
+				}
+				else{
+					res.status(500).jsonp({"error" : errLoadSeries});
+				}
+			});
+		}
+		else {
+			res.status(500).jsonp({"error" : "We could't find your user token"});
+		}
+	});
 };
 
 
 exports.getEpisodeDetails = function(req, res){
-	console.log('in getEpisodeDetails');
+
+	var episodeId = req.params.episodeId;
+
+	tokenController.verify(req.params.token, function (verified, user) {
+		if (verified) {
+			if(clog) console.log("Access to restricted area granted");
+			
+			Series.findOne({'Episode.id':episodeId}, function (errLoadSeries, resultSeries){
+				if(resultSeries && !errLoadSeries){
+
+					var foundInEpisodes = -1;
+
+					for(var i=0; i<resultSeries.Episode.length; i++){
+						if(resultSeries.Episode[i].id == episodeId){
+							foundInEpisodes = i;
+							break;
+						}
+					}
+
+					if(foundInEpisodes >= 0){
+						res.send(resultSeries.Episode[foundInEpisodes]);
+					}
+					else{
+						res.status(500).jsonp({"error":"Error: episodeId not found"});
+					}
+				}
+				else if(resultSeries === null){
+					res.status(500).jsonp({"error":"Error: episodeId not found"});
+				}
+				else{
+					res.status(500).jsonp({"error" : errLoadSeries});
+				}
+			});
+		}
+		else {
+			res.status(500).jsonp({"error" : "We could't find your user token"});
+		}
+	});
 };
